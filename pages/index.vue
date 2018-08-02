db5df4169a48634f73bdd3afb5c028c783f9ad68
dcversus <dcversus@gmail.com>
2018-08-02 17:40:18 +0300

    store posts in generate

--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,9 @@
 <template>
   <div>
-    <span v-if="!this.loaded">Loading...</span>
+    <span v-if="!this.$store.state.loaded">Loading...</span>
 
     <div class="list">
-      <a :href="post.data.url" v-for="post in posts" :key="post.data.permalink">{{post.data.title}}</a>
+      <a :href="post.data.url" v-for="post in $store.state.posts" :key="post.data.permalink">{{post.data.title}}</a>
     </div>
   </div>
 </template>
@@ -21,10 +21,9 @@ export default {
     posts: [],
     loaded: false,
   }),
-  async created() {
-    const response = await this.$axios.get('https://www.reddit.com/r/hyperloot.json');
-    this.posts = response.data.data.children;
-    this.loaded = true;
+  async fetch({ $axios, store }) {
+    const response = await $axios.get('https://www.reddit.com/r/hyperloot.json');
+    store.commit('posts', response.data.data.children);
   },
 }
 </script>
--- store/index.js
+++ store/index.js
@@ -3,8 +3,14 @@ import Vuex from 'vuex'
 const createStore = () => {
   return new Vuex.Store({
     state: {
+      posts: [],
     },
     mutations: {
+      posts(state, posts) {
+        // изменяем состояние
+        state.posts = posts;
+        state.loaded = true;
+      }
     }
   })
 }
