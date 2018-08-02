3f567fdf6f8257934e8e80c427daf3800412a86c
dcversus <dcversus@gmail.com>
2018-08-02 17:22:38 +0300

    hyperloot news list exp#1

--- pages/index.vue
+++ pages/index.vue
@@ -1,19 +1,30 @@
 <template>
   <div>
-    Hello world
+    <span v-if="!this.loaded">Loading...</span>
+
+    <div class="list">
+      <a :href="post.data.url" v-for="post in posts" :key="post.data.permalink">{{post.data.title}}</a>
+    </div>
   </div>
 </template>
 
 <style scoped>
-
+  .list a {
+    display: block;
+  }
 </style>
 
 <script>
 export default {
   name: 'Normal',
+  data: () => ({
+    posts: [],
+    loaded: false,
+  }),
   async created() {
     const response = await this.$axios.get('https://www.reddit.com/r/hyperloot.json');
-    console.log('response', response.data)
+    this.posts = response.data.data.children;
+    this.loaded = true;
   },
 }
 </script>
